efdcf18e12d3b8a035aaa2f88d3dc9219d4738b3
username <91468479+Davidkim4663@users.noreply.github.com>
2022-02-23 18:09:29 +0900

    python_d1
    comment, identifier, keyword etc..

---
+++ 01_Python/00_Self-studying/day01/comment.py
@@ -0,0 +1,14 @@
+#간단히 출력하는 예
+print("Hello Python Programming") #문자열을 출력 합니다. 
+
+#하나만 출려하기
+print(52)
+print(273)
+
+#여러개 출력하기
+print(52,273,"Hello")
+print("Hello","My","Name","is","David")
+
+#줄바꿈하기
+print()
+    # 빈줄을 출력 합니다.4
---
+++ 01_Python/00_Self-studying/day01/self_coding.py
@@ -0,0 +1,17 @@
+#하나만 출력 합니다.
+print("식별자(identifer)가 소문자로 시작 하므로 Snake case 이며 괄호가 있으므로 function 입니다.")
+print("# 하나만 출력 합니다") 
+print("Hello Python is fun..!") 
+print()
+
+#여러 개를 출력 합니다.
+print("식별자(identifer)가 소문자로 시작 하므로 Snake case 이며 괄호가 있으므로 function 입니다.")
+print("# 여러 개를 출력 합니다")
+print(10,20,30,40,50)
+print("안녕하세요","저의","이름은","김민성 입니다!")
+print()
+
+#아무것도 입력 하지 않으면 단순하게 줄바꿈 합니다.
+print("식별자(identifer)가 소문자로 시작 하므로 Snake case 이며 괄호가 있으므로 function 입니다.")
+print("# 아무것도 출력하지 않습니다")
+print("--확인 전용선--")
